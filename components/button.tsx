eac7fe60c831fd56a5b9348ca2bc7ea7459b7075
junho <junho@2isys.com>
2023-11-23 17:07:48 +0900

    모든역 선택 버튼 추가

--- components/button.tsx
+++ components/button.tsx
@@ -40,7 +40,7 @@ export default function Button({
 	const sizes: Sizes = {
 		none: "",
 		xs: "text-xs px-1 py-0.5",
-		sm: "text-sm px-2 py-1.5",
+		sm: "text-sm px-2 py-1",
 		md: "text-base px-4 py-2",
 		lg: "text-lg px-5 py-1.5",
 	};
